0918d7e51c7fab1d8187dfc6ade8e8d45d5fefc6
Catalina Villablanca <catuga@Catalinas-MacBook-Pro.local>
2024-04-21 20:35:57 -0400

    fixed dialog full screen, made component for image view

--- src/app/img/[id]/page.tsx
+++ src/app/img/[id]/page.tsx
@@ -1,7 +1,15 @@
+import FullPageImageView from '~/components/full-image-page';
+
 export default function PhotoModal({
     params: { id: photoId },
 }: {
     params: { id: string };
 }) {
-    return <div>{photoId}</div>;
+    const idAsNumber = Number(photoId);
+
+    return (
+        <div className="flex h-full min-h-0 w-full min-w-0 overflow-y-hidden">
+            <FullPageImageView id={idAsNumber} />
+        </div>
+    );
 }
