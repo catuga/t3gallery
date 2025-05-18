ff9f7ee69ca672d400f27625c5147415a98fb6ad
catuga <cvillblabl@gmail.com>
2025-05-18 13:48:28 +0100

    finalize modal and parallel image route

--- src/components/full-image-page.tsx
+++ src/components/full-image-page.tsx
@@ -1,15 +1,31 @@
+import { clerkClient } from "@clerk/nextjs/server";
 import { getImage } from "~/server/queries";
 
 export default async function FullPageImageView(props: { id: number }) {
-  const image = await getImage(props.id);
-  return (
-    <div className="bg-green flex h-full w-full min-w-0">
-      <div className="flex-shrink">
-        <img src={image.url} className="object-contain" />
-      </div>
+  const idAsNumber = Number(props.id);
+  if (Number.isNaN(idAsNumber)) throw new Error("Invalid photo id");
 
-      <div className="flex w-48 flex-col">
-        <div className="text-xl font-bold">{image.name}</div>
+  const image = await getImage(idAsNumber);
+
+  const userInfo = await clerkClient.users.getUser(image.userId);
+
+  return (
+    <div className="flex h-full w-screen min-w-0 items-center justify-center text-white">
+      <div className="flex-shrink flex-grow">
+        <img src={image.url} className="object-contain" alt={image.name} />
+      </div>
+      <div className="flex h-full w-56 flex-shrink-0 flex-col border-l">
+        <div className="border-b p-2 text-center text-xl">{image.name}</div>
+
+        <div className="p-2">
+          <div>Uploaded By:</div>
+          <div>{userInfo.fullName}</div>
+        </div>
+
+        <div className="p-2">
+          <div>Created On:</div>
+          <div>{image.createdAt.toLocaleDateString()}</div>
+        </div>
       </div>
     </div>
   );
